fcd1b715eee9fce73f929249cbc3eeefba39b52f
Stephen Moon <stephen.moon@gmail.com>
2023-02-03 20:08:47 -0800

    start on tens digit exercise

--- src/com/vibfft/TensDigitExercise.java
+++ src/com/vibfft/TensDigitExercise.java
@@ -12,15 +12,9 @@ public class TensDigitExercise extends Exercise {
         level = ExLevel.EASY;
     }
 
-    private int formulateEvaluate(int number) {
-        return (((number + 1) * number + 2) * number + 3);
-    }
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         TensDigitExercise o = new TensDigitExercise();
         System.out.println(o.toString(o.type, o.level));
-
-        System.out.println(o.formulateEvaluate(s.nextInt()));
     }
 }
