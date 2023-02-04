173b1dcb461d96c073227c4848c3fe2669fb44c4
Stephen Moon <stephen.moon@gmail.com>
2023-02-03 19:54:44 -0800

    completed timedifference problem

--- src/TimeDifferenceExercise.java
+++ src/TimeDifferenceExercise.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class TimeDifferenceExercise extends Exercise {
 
     ExType type;
@@ -10,6 +12,31 @@ public class TimeDifferenceExercise extends Exercise {
         level = ExLevel.EASY;
     }
 
+    private int secondDifference(String[] strArray) {
+        int firstTime = 0, secondTime = 0;
+        int[] intArray = new int[strArray.length];
+
+        for (int i = 0; i < strArray.length; i++) {
+            if ((i % 3) == 0) {
+                // hour
+                intArray[i] = 60 * 60 * parseInt(strArray[i]);
+            } else if ((i % 3) == 1) {
+                // minute
+                intArray[i] = 60 * parseInt(strArray[i]);
+            } else {
+                // second
+                intArray[i] = parseInt(strArray[i]);
+            }
+        }
+        for (int i = 0; i < intArray.length; i++) {
+            if (i < 3) {
+                firstTime += intArray[i];
+            } else {
+                secondTime += intArray[i];
+            }
+        }
+        return secondTime - firstTime;
+    }
 
     public static void main(String[] args) {
 
@@ -22,15 +49,16 @@ public class TimeDifferenceExercise extends Exercise {
                 "seconds, and the other three defining the second one.");
         System.out.println("Please enter six integers: ");
 
-        String[] stringArray = new String [6];
-        for(int i = 0; i < stringArray.length; i++) {
+        String[] stringArray = new String[6];
+        for (int i = 0; i < stringArray.length; i++) {
             stringArray[i] = s.nextLine();
-            System.out.println("Each Input: " + stringArray[i]);
         }
 
         System.out.println("\nYou have entered: ");
-        for(String str: stringArray) {
+        for (String str : stringArray) {
             System.out.println(str);
         }
+
+        System.out.println(o.secondDifference(stringArray));
     }
 }
